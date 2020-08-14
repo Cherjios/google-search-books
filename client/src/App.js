86067286e92e78aea628639eb18f00326304a483
cherjios <slopez0286@gmail.com>
2020-08-14 13:37:40 -0700

    adding result card to components

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,26 @@ import "./App.css";
 import Navbar from "./components/navbar"
 import Jumbotron from "./components/Jumbotron";
 import FormSearchBook from "./components/Form"
+import ResultCard from "./components/ResultCard"
 
 function App() {
   return (
    //Start adding elements here
    <div>
      <Navbar />
-     <div className="col">
+     <div className="container">
      <Jumbotron />
      <FormSearchBook />
+     <br />
+       <h2>Results!</h2>
+       <div>
+         <ResultCard />
+       </div>
+
+
      </div>
+     
+     
    
 
 
---
+++ client/src/components/ResultCard/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css"
+
+function ResultCard (){
+    return (
+<h1>Hello</h1>
+    );
+}
+
+export default ResultCard
