0fa199df53098eda67983628cf3ce98f94ccb36c
cherjios <slopez0286@gmail.com>
2020-08-14 13:23:47 -0700

    Adding type test to form to serach a book

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -7,8 +7,8 @@ function FormSearchBook() {
 
       <form>
         <div class="form-group">
-          <label for="exampleInputEmail1">Search a book!</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <label for="exampleInputEmail1"><h1>Search a book!  </h1></label>
+          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
         </div>
       </form>
     </div>
