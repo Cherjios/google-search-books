5b9de1e78b943bd990bf60f2c18c2b3c7b3dad94
cherjios <slopez0286@gmail.com>
2020-08-14 13:01:52 -0700

    Adding component to index

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Jumbotron (){
+    return (
+        <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">(REACT)Google Book Search</h1>
+    <p class="lead">Search for and Save Books of Interest.</p>
+  </div>
+</div>
+    );
+}
+
+export default Jumbotron;
--- routes/api/SeachBook.js
+++ routes/api/SeachBook.js
@@ -1,22 +1,25 @@
 const router = require("express").Router();
-const booksController = require("../../controllers/booksController");
+const SearchBooksControllers = require("../../controllers/SearchBooksControllers");
+const router = require("express").Router();
+
 
 // Matches with "/api/books"
 //Should return all saved books as JSON.
 router.route("/api/books")
-  .get(booksController.findAll)
+  .get(SearchBooksControllers.findAll)
   //Will be used to save a new book to the database.
-  .post(booksController.create);
+  .post(SearchBooksControllers.create);
 
 // Matches with "/api/books/:id"
-router
-  .route("/api/books/:id")
-  //Will be used to delete a book from the database by Mongo `_id`.
-  .delete(booksController.remove);
+// router
+//   .route("/api/books/:id")
+//   //Will be used to delete a book from the database by Mongo `_id`.
+//   .delete(SearchBooksControllers.remove);
 
 //will be used to make an api call to `https://www.googleapis.com/books/v1/volumes?q=<Book Name>` 
 //and return the relevant results.
 router.route("/google")
+.googleBookSearch(BookName);
 
 
 
