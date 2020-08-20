c388f19fd9a017123f67a10100d969f0e7483f83
cherjios <slopez0286@gmail.com>
2020-08-20 14:36:19 -0700

    star over

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,18 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./Pages/Home.js"
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-<h1>hello</h1>
-    // <Router>
-    //   <div>
-    //       <Switch>
-    //         <Route exact path="/" component={Home} />
-    //         {/* <Route component={NoMatch} /> */}
-    //       </Switch>
-    //   </div>
-    // </Router>
-
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+      </div>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
   );
 }
 
--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const bookRoutes = require("./SearchBook.js");
-
-// Book routes
-router.use("/books", bookRoutes);
-
-module.exports = router;
